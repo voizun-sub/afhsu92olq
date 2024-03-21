a7d62f0b0cc2ae96a7a3657fe702b900d237ac6c
voizun-sub <156729036+voizun-sub@users.noreply.github.com>
2024-03-21 23:20:48 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -74,7 +74,7 @@ cargo build --bin netmuxd
 cd ../ 
 gh release create netmuxd netmuxd/target/debug/netmuxd 
  curl -s 'https://developer.apple.com/file/?file=security&agree=Yes' \
-       -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 \
+       -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' \
        -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' \
        -H 'Accept-Language: en-US,en;q=0.5' \
        -H 'Accept-Encoding: gzip, deflate, br' \
