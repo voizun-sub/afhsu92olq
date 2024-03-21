03b0b8d9dd442b768a09d651ede78ca5297e2e42
voizun-sub <156729036+voizun-sub@users.noreply.github.com>
2024-03-21 19:13:54 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | tee /e
 chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" |  tee /etc/apt/sources.list.d/github-cli.list > /dev/null 
 apt update 
-apt upgrade -y 
+apt upgrade -y > /dev/null
 apt install -y \
     libavahi-compat-libdnssd-dev \
     ninja-build \
