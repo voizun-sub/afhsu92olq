616d8126a4ee815e2fdb1fb1857f51b4c007c13d
voizun-sub <156729036+voizun-sub@users.noreply.github.com>
2024-03-22 10:37:53 +0900

    Update start.sh

--- wheel/start.sh
+++ wheel/start.sh
@@ -12,4 +12,4 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 rustup toolchain install stable 
 rustup default stable 
-pip wheel --no-cache --wheel-dir=./whl libmobiledevice
+pip wheel --no-cache --wheel-dir=./whl libmobiledevice3
