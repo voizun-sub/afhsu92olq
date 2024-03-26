d6aed28cc59b176aa9c3221c5c2ba9c912f325ef
voizun-sub <156729036+voizun-sub@users.noreply.github.com>
2024-03-26 23:40:39 +0900

    Update wheel-start.sh

--- wheel-start.sh
+++ wheel-start.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
+export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
 apt update 
 apt upgrade -y
-apt install -y python3-pip
-pip3 install --upgrade -y pip setuptools wheel
-pip download -d ./wheel pymobiledevice3
+apt install -y --install-recommends \
+    python3-pip \
+    libopenblas-dev \
+    python3-cvxopt \
+    python3-freetype \
+    python3-hdf* \
+    cmake \
+    pkg-config \
+    libdbus-1-dev \
+    libdbus-1-3 \
+    libglib2.0-dev \
+    build-essential
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y 
+. "$HOME/.cargo/env"
+rustup toolchain install stable 
+rustup default stable
+python -m venv .venv
+.venv/Scripts/acrivate
+pip3 install -y pymobiledevice3
+pip3 freeze > requirements.txt
+pip3 install -y -U pip setuptools wheel
+pip wheel --no-cache --wheel-dir=./wheel -r requirements.txt
+mv requirements.txt ./wheel
